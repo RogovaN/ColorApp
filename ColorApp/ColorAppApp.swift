cf61e035b6dfcf2ce5bd7cadc58852f5fd34308c
Rogova Natalia <rogova.n@icloud.com>
2021-06-07 18:47:39 +0200

    Initial Commit

---
+++ ColorApp/ColorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppApp.swift
+//  ColorApp
+//
+//  Created by Наталья Рогова on 07.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
